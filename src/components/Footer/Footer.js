2aea0e60d89d34797775022e265e5d7fd5245b7b
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-06 18:22:02 +0800

    feat: Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()}. <span>SimpleRecipes</span> Built with{' '}
+        <a href="https://www.gatsbyjs.com/">Gatsby</a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
