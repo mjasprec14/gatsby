c61d317cc45fcb72478c56e77a5140e2a74861ec
Michael Joseph Asprec <mjasprec@Michaels-MacBook-Pro.local>
2023-06-06 13:16:27 +0800

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
-import * as React from "react"
+import * as React from 'react';
+import { Link } from 'gatsby';
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from '../components/ui/navigation-menu';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <NavigationMenu>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          <Link to="/">Home</Link>
+        </NavigationMenuLink>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          <Link to="/about">About</Link>
+        </NavigationMenuLink>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          <Link to="/contact">Contact</Link>
+        </NavigationMenuLink>
+      </NavigationMenu>
+
+      <h1 className="text-red-500">Hello awesome people!!</h1>
+      <Link to="/about">About</Link>
+    </div>
+  );
 }
