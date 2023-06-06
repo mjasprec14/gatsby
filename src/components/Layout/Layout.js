e991664d8fff462df8a845426004eb6d97c7609c
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-06 18:21:43 +0800

    feat: applied stylings to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 
+import '../../assets/css/main.css';
+
 const Layout = ({ children }) => {
   return (
     <>
