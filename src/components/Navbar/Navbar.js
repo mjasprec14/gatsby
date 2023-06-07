38683c6653d8e0be324a6ff9d9cb08606dc61466
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-07 16:50:52 +0800

    feat: Navbar component styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,89 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import {
-  NavigationMenu,
-  NavigationMenuLink,
-  navigationMenuTriggerStyle,
-} from '../ui/navigation-menu';
+import { NavigationMenu, NavigationMenuLink } from '../ui/navigation-menu';
+import { Button } from '../ui/button';
+
+import { RxTextAlignJustify } from 'react-icons/rx';
+import logo from '../../assets/images/recipes/logo.svg';
 
 const Navbar = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-        <Link to="/">Home</Link>
-      </NavigationMenuLink>
-      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-        <Link to="/about">About</Link>
-      </NavigationMenuLink>
-      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-        <Link to="/contact">Contact</Link>
-      </NavigationMenuLink>
-      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-        <Link to="/tags">Tags</Link>
-      </NavigationMenuLink>
-      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-        <Link to="/recipes">Recipes</Link>
-      </NavigationMenuLink>
+    <NavigationMenu className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <NavigationMenuLink>
+            <Link to="/">
+              <img
+                src={logo}
+                alt="logo"
+              />
+            </Link>
+          </NavigationMenuLink>
+
+          <Button
+            variant={'outline'}
+            className="nav-btn"
+          >
+            <RxTextAlignJustify size={25} />
+          </Button>
+        </div>
+        <div className="nav-links show-links">
+          <NavigationMenuLink>
+            <Link
+              className="nav-link"
+              activeClassName="active-link"
+              to="/"
+            >
+              Home
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink>
+            <Link
+              className="nav-link"
+              activeClassName="active-link"
+              to="/about"
+            >
+              About
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink>
+            <Link
+              className="nav-link"
+              activeClassName="active-link"
+              to="/contact"
+            >
+              Contact
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink>
+            <Link
+              className="nav-link"
+              activeClassName="active-link"
+              to="/tags"
+            >
+              Tags
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink>
+            <Link
+              className="nav-link"
+              activeClassName="active-link"
+              to="/recipes"
+            >
+              Recipes
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink className="nav-link contact-link">
+            <Link
+              className="btn"
+              activeClassName="active-link"
+              to="/recipes"
+            >
+              Contact
+            </Link>
+          </NavigationMenuLink>
+        </div>
+      </div>
     </NavigationMenu>
   );
 };
