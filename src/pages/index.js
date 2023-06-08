b49fce9200c9be58da815901ce0cdbd1a05f13c7
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-08 18:19:30 +0800

    feat: home page banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,28 @@
 import * as React from 'react';
 import { Layout } from '../components';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Hello from Gatsby</h1>
+      <main className="mx-auto min-h-[90vh] w-[90vw] max-w-[1120px] ">
+        <header className="relative h-[40vh] mb-8">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="home page banner"
+            className="h-full rounded-sm"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+
+          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/40 rounded-sm">
+            <div className="text-white text-center">
+              <h1>Simple recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   );
 }
