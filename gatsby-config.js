8a3cfef4db663f8b10b75f8a9565651d4c274573
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-09 21:24:18 +0800

    feat: source file system plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,29 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: 'Simple Recipes',
+    description: 'Nice and clean recipes site',
+    author: 'MJ Asprec',
+    person: { name: 'John', age: 32 },
+    simpleData: ['item 1', 'item 2'],
+    complexData: [
+      { name: 'John', age: 32 },
+      { name: 'Jane', age: 42 },
+    ],
+  },
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-root-import',
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 };
