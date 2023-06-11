adf13f2ad8bca844fd2149734b14750f00debd6f
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-11 19:29:37 +0800

    feat: init new components AllRecipes, RecipesList and TagList

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Footer } from './Footer/Footer';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Layout } from './Layout/Layout';
+export { default as AllRecipes } from './AllRecipes/AllRecipes';
+export { default as RecipesList } from './RecipesList/RecipesList';
+export { default as TagList } from './TagList/TagList';
