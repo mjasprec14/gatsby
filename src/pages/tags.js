298f408c32e77d61bd70dfbdc835a7c19762c89e
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-12 19:59:20 +0800

    feat: tags page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import { Layout } from 'components';
+import { graphql, Link } from 'gatsby';
+import setupTags from 'utils/setupTags';
 
-export default function Tags() {
+export const query = graphql`
+  {
+    allContentfulRecipe(sort: { title: ASC }) {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`;
+
+export default function Tags({
+  data: {
+    allContentfulRecipe: { nodes: recipes },
+  },
+}) {
+  const newTags = setupTags(recipes);
   return (
     <Layout>
-      <h1>Tags</h1>
+      <main className="max-w-[1120px] mx-auto w-[90vw] min-h-screen">
+        <section className="grid sm:grid-cols-3 gap-8 pb-12">
+          {newTags.map((tag, index) => (
+            <Link
+              to={`/${tag[0]}`}
+              key={index}
+              className="bg-gray-400 rounded-lg text-white text-center p-3"
+            >
+              <h5 className="mb-0">{tag[0]}</h5>
+              <p>{tag[1]} recipe</p>
+            </Link>
+          ))}
+        </section>
+      </main>
     </Layout>
   );
 }
