d2d3adb3dfe5ca16fda75e1b280c415715490560
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-12 17:59:08 +0800

    feat: TagList Component

--- src/components/TagList/TagList.js
+++ src/components/TagList/TagList.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import setupTags from 'utils/setupTags';
 
-export default function TagList({ tags }) {
-  console.log(tags);
-  return <div>TagList</div>;
+export default function TagList({ recipes }) {
+  const newTags = setupTags(recipes);
+  console.log(newTags);
+  console.log(newTags?.pancakes);
+  return (
+    <section>
+      <h5>Recipes</h5>
+      {Object.keys(newTags)
+        .sort((a, b) => {
+          const [firstTag] = a;
+          const [secondTag] = b;
+          return firstTag.localeCompare(secondTag);
+        })
+        .map((tagName) => (
+          <p>
+            {tagName.charAt(0).toUpperCase()}
+            {tagName.slice(1)} ({newTags[tagName]})
+          </p>
+        ))}
+    </section>
+  );
 }
