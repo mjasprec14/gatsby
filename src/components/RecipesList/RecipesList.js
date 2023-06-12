b3a5ee970d32a90f55453b4c8904c7f61dbf43b9
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-06-12 16:23:35 +0800

    feat: RecipesList Component

--- src/components/RecipesList/RecipesList.js
+++ src/components/RecipesList/RecipesList.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
-export default function RecipesList({ recipes }) {
-  console.log(recipes);
-  return <div>RecipesList</div>;
+export default function RecipesList({ recipes = [] }) {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-8 gap-x-4 pb-12">
+      {recipes?.map(({ id, title, image, prepTime, cookTime }) => {
+        const pathToImage = getImage(image);
+        return (
+          <Link
+            to={`/${title?.toLowerCase().replace(' ', '-')}`}
+            key={id}
+          >
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="h-60 rounded-md "
+            />
+            <h5>{title}</h5>
+            <p className="">
+              Prep: {prepTime}min | Cook: {cookTime}min
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
