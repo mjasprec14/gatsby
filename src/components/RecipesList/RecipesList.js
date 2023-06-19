dab9c43761aa4f72dc56d8dfc6d1c155545c6d53
MJAsprec <michael.asprec@whitecloak.com>
2023-06-19 21:08:56 +0800

    feat: Slugify urls

--- src/components/RecipesList/RecipesList.js
+++ src/components/RecipesList/RecipesList.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import slugify from 'slugify';
 
 export default function RecipesList({ recipes = [] }) {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-4 pb-12">
       {recipes?.map(({ id, title, image, prepTime, cookTime }) => {
         const pathToImage = getImage(image);
+        const slug = slugify(title, { lower: true });
         return (
           <Link
-            to={`/${title?.toLowerCase().replace(' ', '-')}`}
+            to={`/${slug}`}
             key={id}
           >
             <GatsbyImage
